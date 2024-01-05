3e10ab2183c19dfc358211287d86e881f4e24d44
Siham Chari <charisiham22@gmail.com>
2024-01-05 03:03:07 +0100

    cleaned the executable files

--- hello_cargo/target/debug/deps/hello_cargo-9add631b3a00e272.d
+++
@@ -1,5 +0,0 @@
-/home/omotoye/rust_tutorial/hello_cargo/target/debug/deps/libhello_cargo-9add631b3a00e272.rmeta: src/main.rs
-
-/home/omotoye/rust_tutorial/hello_cargo/target/debug/deps/hello_cargo-9add631b3a00e272.d: src/main.rs
-
-src/main.rs:
--- hello_cargo/target/debug/hello_cargo.d
+++
@@ -1,1 +0,0 @@
-/home/omotoye/rust_tutorial/hello_cargo/target/debug/hello_cargo: /home/omotoye/rust_tutorial/hello_cargo/src/main.rs
